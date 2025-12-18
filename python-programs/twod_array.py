49739e8d63bb3ccf44640bd670d97f8d74c10d83
Parashuram <parashurampawar56@gmail.com>
2025-12-18 21:50:35 +0530

    modify program [Parashuram]: two d array

--- python-programs/twod_array.py
+++ python-programs/twod_array.py
@@ -1,15 +1,36 @@
+def two_d_array_input(rows, columns, datatype):
+  arr = []
+
+  #input statement
+  print("Enter the elements row wise: ")
+  for i in range(rows):
+    l = list(map(datatype, input().split()))
+    arr.append(l)
+
+  #print statement
+  print("The 2D array elements are: ")
+  for i in range(rows):
+    for j in range(columns):
+      print(arr[i][j], end=" ")
+    print()
+
+
+
+
+
 rows = int(input("Enter number of rows: "))
 columns = int(input("Enter number of columns: "))
 
-arr = []
-print("Enter the elements row wise: ")
-for i in range(rows):
-  row = list(map(int, input().split()))
-  arr.append(row)
-
-print("The 2D array elements are: ")
-for i in range(rows):
-  for j in range(columns):
-    print(arr[i][j], end=" ")
-  print()
-    
+datatype = input("Enter the type of data you want to store (int/float/boolean): ").lower()
+if(datatype == 'int'):
+  two_d_array_input(rows, columns, int)
+elif(datatype == 'float'):
+  two_d_array_input(rows, columns, float)
+elif(datatype == 'boolean'):
+  if datatype in ("true","t","1","yes","y"):
+    boolean_val = True
+  elif datatype in ("false","f","0","no","n"):
+    boolean_val = False 
+  two_d_array_input(rows, columns, boolean_val)
+else:
+  print("Invalid data type selected.")
